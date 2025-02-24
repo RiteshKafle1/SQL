60952f7fbc71c918d97ac70ab77c6a7480b321f8
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-02-24 17:35:06 +0545

    datatype and creation of table

--- learning#1.sql
+++ learning#1.sql
@@ -1,14 +1,15 @@
+-- Active: 1740393092337@@127.0.0.1@3306@learning
 -- This is the way to create a database
 -- CREATE DATABASE db_name;
 
--- view all the databases
+-- view all the database
 -- SHOW DATABASES;
 
 -- way to delete a database.
 -- DROP DATABASE db_name;
 
 -- use a database.
--- USE db_name;
+-- USE LEARNING;
 
--- show the current db name.
+-- show the current db you are using.
 -- SELECT DATABASE() 
---
+++ learning#2.sql
@@ -0,0 +1,9 @@
+-- Table -> collection of data organized in rows and columns
+
+-- DataType -> tells the db what kind of information can be stored in a particular column.
+
+-- CREATE A TABLE (STUDENT) WITH PROPERTIES OR COLUMN :NAME AND ROLL .VARCHAR ? -> TAKES THE NECESSARY SIZE OR LENGTH.
+CREATE TABLE STUDENT(
+  NAME VARCHAR(30),
+  ROLL INT
+)
