8609a6703bba86ac10edfaf894620414f54b11b2
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-02-25 15:41:12 +0545

    inserting and selecting from a table

---
+++ learning#3.sql
@@ -0,0 +1,18 @@
+CREATE TABLE MOVIES(
+  name VARCHAR(20),
+  release_year INT DEFAULT 2025,
+  ratings int Default 0
+);
+
+-- INSERT DATA INTO THE TABLE AND DISPLAT IT
+
+-- INSERT INTO movies(name,release_year,ratings)
+-- VALUES('mahadev',2026,5),('shreeram',2025,5)
+-- SELECT * FROM movies;
+
+-- TO SELECT A SPECIFIC FIELD
+-- SELECT name from movies
+
+
+
+
